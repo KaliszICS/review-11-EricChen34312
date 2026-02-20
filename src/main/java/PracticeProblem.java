970a3cdb98d2dc87af14001bc05385402969ef2e
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-02-20 14:19:21 +0000

    Solve Q1-Q4

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,44 @@ public class PracticeProblem {
 
 	}
 
+	public static int[] createIntArray(){
+		//initiate and populate array arr
+		int[] arr = new int[5];
+		for(int i = 0; i < 5; i++){
+			arr[i] = i+1;
+		}
 
+		return arr;
+	}
+
+	public static String[] createArray(String a, String b, String c, String d){
+		String[] arr = {a,b,c,d};
+		return arr;
+		//???????
+	}
+
+	public static int findValue(int x,int[] arr){
+		//return index if found, else get outta there
+		for(int i = 0; i < arr.length;i++){
+			if(arr[i] == x){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findThirdValue(String str, String[] arr){
+		int count = 0;
+		//with counter, increment every time str in arr, return index if counter reaches 3
+		for(int i = 0; i < arr.length; i++){
+			if (arr[i].equals(str)){
+				count++;
+			}
+
+			if (count >= 3){
+				return i;
+			}
+		}
+		return -1;
+	}
 }
